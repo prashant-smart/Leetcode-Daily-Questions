b41826af23159d5177c29f0554379f840009d463
PRASHANT KUMAR YADAV <Kartik224blue@gmail.com>
2022-06-23 10:36:58 +0530

    Time: 270 ms (46.69%), Space: 11.4 MB (59.59%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
-    
- string longestPalindrome(string s) {
-        int end=1; int start=0;
-        for(int i=1; i<s.length();i++){
-            
-            //even logic
-            int h=i-1,l=i;
-            while(i>=0 && l<s.length()&& s[h]==s[l]){
-                if(h-l+1>end){
-                    end=h-l+1;
-                    start=l;
-                }
-                h++;
-                l--;
+    string longestPalindrome(string s) {
+        int n=s.length();
+        int dp[n+1][n+1];
+         memset(dp,0,sizeof(dp));
+         int I=0,J=0;
+        for(int i=0;i<s.length();i++){
+            dp[i][i]=1;
+            if(i!=s.length()-1&&s[i]==s[i+1]){
+                dp[i][i+1]=1;
+                I=i;
+                J=i+1;
             }
-            
-            //odd logic
-             h=i-1,l=i+1;
-            while(i>=0 && l<s.length()&& s[h]==s[l]){
-                if(h-l+1>end){
-                    end=h-l+1;
-                    start=l;
-                }
-                h++;
-                l--;
-            }
-            
         }
-        return s.substr(start,end);
         
+       
+        
+        for(int i=n-3;i>=0;i--){
+            for(int j=i+2;j<n;j++){
+                if(dp[i+1][j-1]&&s[i]==s[j]){
+                    // cout<<i<<" "<<j<<" "<<I<<" "<<J<<endl;
+                    dp[i][j]=1;
+                    if(j-i+1>J-I+1){
+                        J=j;
+                        I=i;
+                    }
+                }
+            }
+        }
+        return s.substr(I,J-I+1);
     }
 };
