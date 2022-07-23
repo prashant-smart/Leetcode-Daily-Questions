466062bae5c3400d6b6d0668426606867edc494b
PRASHANT KUMAR YADAV <Kartik224blue@gmail.com>
2022-07-23 21:32:08 +0530

    Time: 176 ms (100.00%), Space: 107.5 MB (50.00%) - LeetHub

---
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+  
+    long long zeroFilledSubarray(vector<int>& nums) {
+        long long cnt=0;
+        long long ans=0;
+        for(auto i:nums){
+            if(i==0) cnt++;
+            else {
+                ans+=(cnt*(cnt+1))/2;
+                cnt=0;
+            }
+        }
+        ans+=(cnt*(cnt+1))/2;
+        return ans;
+    }
+};
