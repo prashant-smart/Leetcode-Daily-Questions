223c468b66407b078f3d4748b7330ead75e9bb3a
PRASHANT KUMAR YADAV <Kartik224blue@gmail.com>
2022-09-27 23:05:28 +0530

    Time: 612 ms (5.03%), Space: 14.9 MB (80.07%) - LeetHub

---
+++ 838-push-dominoes/838-push-dominoes.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    string pushDominoes(string d) {
+        string ans;
+        for(auto i:d) ans.push_back('.');
+        
+        int l=0;
+        while(l<d.length()){
+            if(d[l]!='.'){
+                if(d[l]=='L'){
+                     int k=l;
+                    while(l>=0&&ans[l]=='.'){
+                            ans[l--]='L';
+                    }
+                    l=k+1;
+                    continue;
+                }
+                
+                int i;
+                for( i=l+1;i<d.length()&&d[i]=='.';i++){
+                    // do nothing eat five star    
+                }
+                if(i==d.length()){
+                    if(d[l]=='R'){
+                        while(l<d.length()&&ans[l]=='.'){
+                            ans[l++]='R';
+                        }
+                    }else{
+                        while(l>=0&&ans[l]=='.'){
+                            ans[l--]='L';
+                        }
+                    }
+                    l=i;
+                }
+                else{
+                    int k=i;
+                    if(d[l]!=d[i]&&d[l]=='R'){
+                        
+                            while(l<i){
+                                ans[l++]='R';
+                                ans[i--]='L';
+                            }
+                        l=k+1;
+                        
+                    }else{
+                        while(l<i){
+                            ans[l++]='R';
+                        }
+                    }
+                    
+                }
+            }else l++;
+            cout<<l<<" ";
+        }
+        return ans;
+    }
+};
